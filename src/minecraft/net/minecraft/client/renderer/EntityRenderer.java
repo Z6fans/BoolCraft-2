49bfd1f5196f7baecee537a9071ca3bbe1871320
Z6fans <z6fans20@gmail.com>
2019-02-08 10:46:46 -0500

    Changed Name of EntityPlayer While Reverting

--- src/minecraft/net/minecraft/client/renderer/EntityRenderer.java
+++ src/minecraft/net/minecraft/client/renderer/EntityRenderer.java
@@ -3,7 +3,7 @@ package net.minecraft.client.renderer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.crash.CrashReport;
 import net.minecraft.crash.ReportedException;
-import net.minecraft.player.EntityPlayerSP;
+import net.minecraft.client.EntityPlayer;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.GL11;
@@ -97,7 +97,7 @@ public class EntityRenderer
                 }
                 
                 this.getMouseOver(partialTickTime);
-                EntityPlayerSP player = this.mc.renderViewEntity;
+                EntityPlayer player = this.mc.renderViewEntity;
                 RenderGlobal renderGlobal = this.mc.renderGlobal;
                 double partialX = player.lastTickPosX + (player.posX - player.lastTickPosX) * (double)partialTickTime;
                 double partialY = player.lastTickPosY + (player.posY - player.lastTickPosY) * (double)partialTickTime;
