df09f0058bb848073ba73350970c474832fdd059
Z6fans <z6fans20@gmail.com>
2020-08-14 00:39:33 -0400

    NibbleArray deobfuscation

--- src/minecraft/net/minecraft/world/chunk/NibbleArray.java
+++ src/minecraft/net/minecraft/world/chunk/NibbleArray.java
@@ -14,51 +14,51 @@ public class NibbleArray
     private final int depthBits;
 
     /**
-     * Log base 2 of the chunk height (128) * width (16); applied as a shift on X coordinate
+     * Log base 2 of the chunk height (128) * width (16); applied as a shift on Y coordinate
      */
     private final int depthBitsPlusFour;
 
-    public NibbleArray(int p_i1992_1_, int p_i1992_2_)
+    public NibbleArray(int length, int depth)
     {
-        this.data = new byte[p_i1992_1_ >> 1];
-        this.depthBits = p_i1992_2_;
-        this.depthBitsPlusFour = p_i1992_2_ + 4;
+        this.data = new byte[length >> 1];
+        this.depthBits = depth;
+        this.depthBitsPlusFour = depth + 4;
     }
 
-    public NibbleArray(byte[] p_i1993_1_, int p_i1993_2_)
+    public NibbleArray(byte[] data, int depth)
     {
-        this.data = p_i1993_1_;
-        this.depthBits = p_i1993_2_;
-        this.depthBitsPlusFour = p_i1993_2_ + 4;
+        this.data = data;
+        this.depthBits = depth;
+        this.depthBitsPlusFour = depth + 4;
     }
 
     /**
      * Returns the nibble of data corresponding to the passed in x, y, z. y is at most 6 bits, z is at most 4.
      */
-    public int get(int p_76582_1_, int p_76582_2_, int p_76582_3_)
+    public int get(int x, int y, int z)
     {
-        int var4 = p_76582_2_ << this.depthBitsPlusFour | p_76582_3_ << this.depthBits | p_76582_1_;
-        int var5 = var4 >> 1;
-        int var6 = var4 & 1;
-        return var6 == 0 ? this.data[var5] & 15 : this.data[var5] >> 4 & 15;
+        int nibbAddr = y << this.depthBitsPlusFour | z << this.depthBits | x;
+        int byteAddr = nibbAddr >> 1;
+        int subAddr = nibbAddr & 1;
+        return subAddr == 0 ? this.data[byteAddr] & 15 : this.data[byteAddr] >> 4 & 15;
     }
 
     /**
      * Arguments are x, y, z, val. Sets the nibble of data at x << 11 | z << 7 | y to val.
      */
-    public void set(int p_76581_1_, int p_76581_2_, int p_76581_3_, int p_76581_4_)
+    public void set(int x, int y, int z, int val)
     {
-        int var5 = p_76581_2_ << this.depthBitsPlusFour | p_76581_3_ << this.depthBits | p_76581_1_;
-        int var6 = var5 >> 1;
-        int var7 = var5 & 1;
+        int nibbAddr = y << this.depthBitsPlusFour | z << this.depthBits | x;
+        int byteAddr = nibbAddr >> 1;
+        int subAddr = nibbAddr & 1;
 
-        if (var7 == 0)
+        if (subAddr == 0)
         {
-            this.data[var6] = (byte)(this.data[var6] & 240 | p_76581_4_ & 15);
+            this.data[byteAddr] = (byte)(this.data[byteAddr] & 240 | val & 15);
         }
         else
         {
-            this.data[var6] = (byte)(this.data[var6] & 15 | (p_76581_4_ & 15) << 4);
+            this.data[byteAddr] = (byte)(this.data[byteAddr] & 15 | (val & 15) << 4);
         }
     }
 }
